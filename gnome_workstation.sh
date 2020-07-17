0702ddd569bb9c93b0a25cc063e5774348b94fca
Natãn <natanmschneider2@gmail.com>
2020-07-17 00:45:43 -0300

    gnome

--- gnome_workstation.sh
+++ gnome_workstation.sh
@@ -1,24 +1,25 @@
-sudo su 
+#!/bin/bash
 
-apt install gnome-tweaks &&
-apt install spotify-client &&
-apt install code &&
-apt install gitkraken &&
-apt install caffeine &&
+sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
+
+sudo apt update && 
+
+apt install gnome-tweaks spotify-client code gitkraken caffeine python3 python-pip nautilus-dropbox docker docker-compose git build-essential -y &&
 apt update 
 
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable &&
-sudo apt-get update && sudo apt-get install qbittorrent
+sudo apt update && sudo apt install qbittorrent -y &&
 
 sudo apt install apt-transport-https curl &&
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - &&
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list &&
 sudo apt update &&
-sudo apt install brave-browser
+sudo apt install brave-browser -y &&
 
 
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - &&
-~/.dropbox-dist/dropboxd
+~/.dropbox-dist/dropboxd -y &&
 
+sudo apt update && sudo apt dist-upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y &&
 
 echo "Pronto!"
